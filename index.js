df9a92023755f043ef1dd7808a061f8cce7e6be8
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-16 07:04:11 +0600

    added mongodb data

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.get("/", (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const appoinmentOptionsCollection = client.db('doctorsPortal').collection('appoinmentOptions');
+
+        app.get("/appoinment-options", async(req, res) => {
+            const query = {};
+            const options = await appoinmentOptionsCollection.find(query).toArray();
+            res.send(options);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 
 
